b8eff6c27e9a0b8122537ca9fa67390b2ec1b57f
dreams <dreamswave.dev@gmail.com>
2022-04-10 21:08:50 +0300

    auto zip

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { FunctionContext, FunctionEvent } from './types';
-
-export const handler = async (event: FunctionEvent, context: FunctionContext) => ({
-    statusCode: 200,
-    headers: {
-        'Content-Type': 'text/plain',
-    },
-    body: 'Hello world!',
-});
