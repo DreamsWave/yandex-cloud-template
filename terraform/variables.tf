0630e8f7c4cea011b700f5e0e308f5cd358715a4
dreams <idreamzzer@gmail.com>
2022-04-10 16:36:24 +0300

    terraform init

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "cloud_id" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "token" {
+  type = string
+}
+
+variable "function_name" {
+  type = string
+}
+
+variable "service_account_id" {
+  type = string
+}
