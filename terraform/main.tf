f126e49a34612806e21f8fe86e89251a789b5211
dreams <idreamzzer@gmail.com>
2022-04-10 23:50:28 +0300

    handling multiple functions deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,24 +19,24 @@ provider "yandex" {
 }
 provider "archive" {}
 
-resource "yandex_function" "example-function" {
-  name               = var.function_name
-  user_hash          = data.archive_file.example-function-archive.output_base64sha256
+resource "yandex_function" "function" {
+  for_each = toset(var.functions)
+  name               = each.key
+  user_hash          = data.archive_file.function-archive[each.key].output_base64sha256
   runtime            = "nodejs16"
   entrypoint         = "index.handler"
   memory             = "128"
   execution_timeout  = "10"
   service_account_id = var.service_account_id
   content {
-    zip_filename = "${path.module}/../tmp/${var.function_name}.zip"
+    zip_filename = "${path.module}/../tmp/${each.key}.zip"
   }
 }
-data "archive_file" "example-function-archive" {
+data "archive_file" "function-archive" {
+  for_each = toset(var.functions)
   type        = "zip"
-  source_dir  = "${path.module}/../functions/${var.function_name}/dist"
-  output_path = "${path.module}/../tmp/${var.function_name}.zip"
+  source_dir  = "${path.module}/../tmp/${each.key}"
+  output_path = "${path.module}/../tmp/${each.key}.zip"
 }
-
-
 
 
