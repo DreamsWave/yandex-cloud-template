8c3b218fdab8cd377bb9fe58ceb89f040977fb4d
idreamzzer <idreamzzer@gmail.com>
2022-04-13 18:55:54 +0300

    added commented examples in terraform and removed tmp folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,18 +2,14 @@
 
 dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-rm -rf $dir/../tmp
-mkdir $dir/../tmp
 cd $dir/../functions
-
 functions=$(cd $dir/../functions && ls -d *)
+
 for f in ${functions}; do
     echo $f
     cd $dir/../functions/$f/
     rm -rf dist
     npx tsc --build tsconfig.json
     cp package.json ./dist/package.json
-    cp -r dist $dir/../tmp/$f
-    cd $dir/../tmp
-    zip -rj $f.zip $f
+    zip -rj build.zip ./dist
 done
